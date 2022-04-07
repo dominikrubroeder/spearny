317a216a30c1b321c2cbfb19341111791ab13902
Dominik Rubröder <dominik.rubroeder@icloud.com>
2022-04-07 11:22:37 +0200

    [firebase-authentication] Set up authentication with Firebase

--- src/pages/Authentication.js
+++ src/pages/Authentication.js
@@ -1,96 +1,82 @@
-import { Fragment, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Fragment, useState, useRef } from 'react';
 import BaseCard from '../components/base/BaseCard';
 import BaseButton from '../components/base/BaseButton';
 
 const Authentication = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [enteredEmailState, setEnteredEmailState] = useState({
-    value: '',
-    isValid: false,
-    wasTouched: false,
-    wasInitial: true,
-  });
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const emailInput = useRef();
+  const passwordInput = useRef();
+  const [isLogin, setIsLogin] = useState(true);
+  const API_KEY = 'AIzaSyCfILVsJgXoj6YygpYtIs_GiLsiJMfxZ8w';
 
-  const checkAuth = (e) => {
+  const switchAuthModeHandler = () => {
+    setIsLogin((previousState) => !previousState);
+  };
+
+  const submitHandler = (e) => {
     e.preventDefault();
-    console.log('Authenticating...');
-    if (enteredEmailState.value.trim() === '' || enteredPassword.trim() === '')
-      return;
-    setIsAuthenticated(true);
-  };
 
-  const handleEnteredEmail = (e) => {
-    const inputValue = e.target.value;
-    setEnteredEmailState((prevState) => {
-      return { ...prevState, value: inputValue };
-    });
+    const enteredEmail = emailInput.current.value;
+    const enteredPassword = passwordInput.current.value;
 
-    // Check if it was just the first click to the input field without entering characters
-    if (setEnteredEmailState.wasInitial) {
-      setEnteredEmailState((prevState) => {
-        return { ...prevState, wasInitial: false };
+    // optional: validation here...
+
+    if (isLogin) {
+      // ...
+    } else {
+      fetch(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            email: enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      ).then((res) => {
+        if (res.ok) {
+          // ...
+        } else {
+          return res.json().then((data) => {
+            // show error modal
+            console.log(data);
+            // further error handling
+          });
+        }
       });
-      return;
     }
 
-    // Only validate further when user added more than 5 characters
-    if (inputValue.length < 5) {
-      return;
-    }
-
-    if (
-      inputValue === '' ||
-      !inputValue.includes('@') ||
-      !inputValue.includes('.')
-    ) {
-      setEnteredEmailState((prevState) => {
-        return { ...prevState, isValid: false, wasTouched: true };
-      });
-      return;
-    }
-    setEnteredEmailState((prevState) => {
-      return { ...prevState, isValid: true };
-    });
-  };
-
-  const handleEnteredPassword = (e) => {
-    setEnteredPassword(e.target.value);
-    checkAuth(e);
+    console.log('...');
   };
 
   return (
     <Fragment>
       <h1>Hello, Spearny</h1>
-      <form className="h-grid" onSubmit={checkAuth}>
+      <form className="h-grid" onSubmit={submitHandler}>
         <BaseCard mode="form-control" background="light">
-          <input
-            type="email"
-            value={setEnteredEmailState.value}
-            placeholder="E-Mailaddress..."
-            onFocus={handleEnteredEmail}
-            onBlur={handleEnteredEmail}
-            onChange={handleEnteredEmail}
-          />
+          <input type="email" placeholder="E-Mailaddress..." ref={emailInput} />
         </BaseCard>
-        {enteredEmailState.wasTouched && !enteredEmailState.isValid && (
-          <p>Please enter a valid e-mail address.</p>
-        )}
         <BaseCard mode="form-control" background="light">
           <input
             type="password"
-            value={enteredPassword}
             placeholder="Password..."
-            onChange={handleEnteredPassword}
+            ref={passwordInput}
           />
         </BaseCard>
-      </form>
-      <Link to="/dashboard">
-        <BaseButton priority="primary" disabled={!isAuthenticated}>
-          Login
+        <BaseButton priority="primary">
+          {isLogin ? 'Login' : 'Sign up'}
         </BaseButton>
-      </Link>
+      </form>
+      <BaseButton
+        priority="primary"
+        mode="link"
+        onClick={switchAuthModeHandler}
+      >
+        {isLogin ? 'Sign up' : 'Login'} instead
+      </BaseButton>
     </Fragment>
   );
 };
