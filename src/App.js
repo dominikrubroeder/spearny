465d25ef737646a08d4775925a4e033c6d74a55e
Dominik Rubröder <dominik.rubroeder@icloud.com>
2022-04-07 09:28:58 +0200

    Migrate react router to version 6, add lazy loading for all current routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import { Fragment } from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import React, { Fragment, Suspense } from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+
 import './font/font.scss';
 import './App.scss';
+
 import TheHeader from './ui/header/TheHeader';
-import Authentication from './pages/Authentication';
-import Dashboard from './pages/Dashboard';
+
+const Authentication = React.lazy(() => import('./pages/Authentication'));
+const Dashboard = React.lazy(() => import('./pages/Dashboard'));
 
 function App() {
   return (
     <Fragment>
       <TheHeader />
       <main className="main-content h-grid">
-        <Route path="/" exact>
-          <Redirect to="/authentication" />
-        </Route>
-        <Route path="/authentication">
-          <Authentication />
-        </Route>
-        <Route path="/dashboard">
-          <Dashboard />
-        </Route>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route
+              path="/"
+              element={<Navigate replace to="/authentication" />}
+            />
+            <Route path="/authentication" element={<Authentication />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </Suspense>
       </main>
     </Fragment>
   );
